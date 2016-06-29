36e70d1284ac72689a87602590badfd38d05daf2
bansalhimanshu75 <himanshu.bansal@kelltontech.com>
2016-06-29 13:51:48 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,18 @@
+############################################################################
+#
+#						Kellton Tech Sol. Lmt
+#						270, Phase 2 
+#						Udyog Vihar
+#						Gurgaon (122001)
+#						Haryana (India)
+############################################################################
+#						Author		: Himanshu Bansal & Mahesh Salaria (RM)
+#						Description : admin.py file is responsible for admin 
+#									  panel used to show the data to admin.
+#						
+############################################################################
+#						Predefined Libraries
+
+from django.contrib import admin
+
+# Register your models here.
